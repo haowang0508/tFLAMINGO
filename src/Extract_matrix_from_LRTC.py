c3498f1ec4fb77171544ad86fdab6c46bdebb00e
Hao Wang <haowang0508@gmail.com>
2022-03-16 20:12:28 -0400

    adding support for different alpha and threshold

--- src/Extract_matrix_from_LRTC.py
+++ src/Extract_matrix_from_LRTC.py
@@ -6,15 +6,19 @@ def Parser():
    parser=argparse.ArgumentParser('')
    parser.add_argument('-i', help = 'input npy path')
    parser.add_argument('-o', help = 'output file path')
+   parser.add_argument('-t', default=1, help = 'minimum imputed IF for valid data')
+   parser.add_argument('-alpha', default=0.25, help = 'alpha')
    return parser.parse_args()   
 args = Parser()
 input_path = args.i
 output_path = args.o
 dat = np.load(input_path)
+threshold = args.t
+alpha = args.alpha
 for idx,mat in enumerate(dat):
     tmp_mat = np.real(mat)
     if_mat = tmp_mat
-    if_mat[if_mat<1] = np.nan
-    pd_mat = if_mat**(-0.25)
+    if_mat[if_mat<threshold] = np.nan
+    pd_mat = if_mat**(-alpha)
     np.savetxt(output_path+'IF_Cell_'+str(idx+1)+'.txt',if_mat)
     np.savetxt(output_path+'PD_Cell_'+str(idx+1)+'.txt',pd_mat)
